5a24ddcffedad4dd1f67a39c24d9799a32050acc
LJBGH <1983810978@qq.com>
2021-11-16 17:29:26 +0800

    [Add]事件总线

---
+++ src/Liliya.Core.API/Event/TestEvent.cs
@@ -0,0 +1,11 @@
+﻿using Liliya.EventBus;
+
+namespace Liliya.Core.API.Event
+{
+    public class TestEvent : EventData
+    {
+        public string Name { get; set; }
+
+        public string Remork { get; set; }
+    }
+}
---
+++ src/Liliya.EventBus/EventBus/EventData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Liliya.EventBus
+{
+    public class EventData : IEventData
+    {
+        public EventData()
+        {
+            EventTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// 事件发生的时间
+        /// </summary>
+        public DateTime EventTime { get ; set ; }
+
+        /// <summary>
+        /// 触发事件的对象
+        /// </summary>
+        public object EventSource { get ; set ; }
+    }
+}
