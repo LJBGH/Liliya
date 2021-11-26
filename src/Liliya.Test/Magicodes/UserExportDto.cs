a684892e0857e3dcc2d3a8ff5a5d42164628d452
LJBGH <1983810978@qq.com>
2021-11-26 18:30:43 +0800

    [Modified]Magicodes测试

---
+++ src/Liliya.Test/Magicodes/UserExportDto.cs
@@ -0,0 +1,67 @@
+﻿using Magicodes.ExporterAndImporter.Core;
+using Magicodes.ExporterAndImporter.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liliya.Test.Magicodes
+{
+    [ExcelExporter(Name="测试",AutoCenter =true,TableStyle = OfficeOpenXml.Table.TableStyles.None)]
+    public class UserExportDto
+    {
+        /// <summary>
+        /// 主键Id
+        /// </summary>
+        [DisplayName("主键Id")]
+        [ExporterHeader(DisplayName ="主键ID",IsAutoFit = true)]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 账号
+        /// </summary>
+        [DisplayName("账号")]
+        [ExporterHeader(DisplayName = "账号", IsAutoFit = true)]
+        public string Account { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        [DisplayName("密码")]
+        [ExporterHeader(DisplayName = "密码", IsAutoFit = true)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 人员姓名
+        /// </summary>
+        [DisplayName("人员姓名")]
+        [ExporterHeader(DisplayName = "人员姓名", IsAutoFit = true)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 工号
+        /// </summary>
+        [DisplayName("工号")]
+        [ExporterHeader(DisplayName = "工号", IsAutoFit = true)]
+        public string JobNumber { get; set; }
+
+        /// <summary>
+        /// 部门
+        /// </summary>
+        [DisplayName("部门")]
+        [ExporterHeader(DisplayName = "部门", IsAutoFit = true)]
+        public string Department { get; set; }
+
+        /// <summary>
+        /// 职位
+        /// </summary>
+        [DisplayName("职位")]
+        [ExporterHeader(DisplayName = "职位", IsAutoFit = true)]
+        public string Position { get; set; }
+
+
+
+    }
+}
--- src/Liliya.Test/UnitTest1.cs
+++ src/Liliya.Test/UnitTest1.cs
@@ -1,6 +1,12 @@
 using Liliya.Models.Entitys.Sys;
+using Liliya.Test.Magicodes;
+using Magicodes.ExporterAndImporter.Core;
+using Magicodes.ExporterAndImporter.Excel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SqlSugar;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Liliya.Test
 {
@@ -8,7 +14,7 @@ namespace Liliya.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SqlSugarCodeFirstTest()
         {
 
             SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
@@ -26,6 +32,43 @@ namespace Liliya.Test
 
             //db.CodeFirst.InitTables(typeof(UserEntity));
             db.CodeFirst.InitTables(typeof(DataDictionaryEntity));
+
+        }
+
+
+
+        [TestMethod]
+        public async Task MagicodesTest()
+        {
+            SqlSugarClient DB = new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = "server=rm-uf61ev7xoo84r6707fo.mysql.rds.aliyuncs.com;userid=ljb_ali123;pwd=Llbali@Password; database=liliya;connectiontimeout=3000;port=3306;Pooling=true;Max Pool Size=300; Maximum Pool Size=1000;sslMode=None;",//连接符字串
+                DbType = DbType.MySql,
+                IsAutoCloseConnection = true,
+                InitKeyType = InitKeyType.Attribute,//从特性读取主键自增信息
+            });
+
+            var users = await DB.Queryable<UserEntity>().Select(x => new UserExportDto
+            {
+                Id = x.Id,
+                Account = x.Account,
+                Password = x.Password,
+                Name = x.Name,
+                JobNumber = x.JobNumber,
+                Department = x.Department,
+                Position = x.Position
+            }).ToListAsync();
+
+            IExporter exporter = new ExcelExporter();
+
+
+            var filePath = $"C://Users//SocialMED-260//Desktop//MyStore//Liliya//src//Liliya.Core.API//wwwroot//export//{DateTime.Now.ToString("yyyyyMMddhhmmss")}.xlsx";
+
+            var result = await exporter.Export(filePath, users);
+
+    
+
+
 
         }
     }
