e98b6f44ee49a7ed051e002182dfdcc757f88da2
LJBGH <1983810978@qq.com>
2021-12-01 14:20:19 +0800

    [Modified]

---
+++ src/Liliya.Test/ScheduleEntity.cs
@@ -0,0 +1,195 @@
+﻿using Liliya.Shared;
+using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liliya.Test
+{
+    /// <summary>
+    /// 任务计划表
+    /// </summary>
+    [SugarTable("Schedule")]
+    //[SplitTable(SplitType.Year)]
+    public class ScheduleEntity : IFullAuditedEntity
+    {
+        /// <summary>
+        /// 主键(任务Id)
+        /// </summary>
+        [SugarColumn(IsPrimaryKey = true/*, IsIdentity = true*/)]
+        [DisplayName("主键(任务Id)")]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 任务名称
+        /// </summary>
+        [DisplayName("任务名称")]
+        public string JobName { get; set; }
+
+        /// <summary>
+        /// 任务分组
+        /// </summary>
+        [DisplayName("任务分组")]
+        public string JobGroup { get; set; }
+
+        /// <summary>
+        /// 任务状态
+        /// </summary>
+        [DisplayName("任务状态")]
+        public JobStatus JobStatus { get; set; }
+
+        /// <summary>
+        /// 任务运行状态
+        /// </summary>
+        [DisplayName("任务运行状态")]
+        public RunStatus RunStatus { get; set; }
+
+        /// <summary>
+        /// 任务运行时间表达式
+        /// </summary>
+        [DisplayName("任务运行时间表达式")]
+        public string Cron { get; set; }
+
+        /// <summary>
+        /// 任务描述
+        /// </summary>
+        [DisplayName("任务描述")]
+        public string Remark { get; set; }
+
+        /// <summary>
+        /// 任务所在DLL对应的程序集名称
+        /// </summary>
+        [DisplayName("任务所在DLL对应的程序集名称")]
+        public string AssemblyName { get; set; }
+
+        /// <summary>
+        /// 任务所在类
+        /// </summary>                                                  0
+        [DisplayName("任务所在类名称")]
+        public string ClassName { get; set; }
+
+        /// <summary>
+        /// 执行次数
+        /// </summary>
+        [DisplayName("执行次数")]
+        public int RunTimes { get; set; }
+
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+        [DisplayName("开始时间")]
+        public DateTime BeginTime { get; set; }
+
+        /// <summary>
+        /// 结束时间
+        /// </summary>
+        [DisplayName("结束时间")]
+        public DateTime? EndTime { get; set; }
+
+        /// <summary>
+        /// 下次执行时间
+        /// </summary>
+        public DateTime? NextTime { get; set; }
+
+        /// <summary>
+        /// 触发器类型
+        /// </summary>
+        [DisplayName("触发器类型")]
+        public TriggerType TriggerType { get; set; }
+
+        /// <summary>
+        /// 执行间隔时间, 秒为单位
+        /// </summary>
+        [DisplayName("执行间隔时间, 秒为单位")]
+        public int IntervalSecond { get; set; }
+
+
+
+
+
+
+        #region   通用字段
+        /// <summary>
+        /// 创建人Id
+        /// </summary>
+        [DisplayName("创建人Id")]
+        public Guid CreatedId { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        [DisplayName("创建时间")]
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// 最后修改人Id
+        /// </summary>
+        [DisplayName("最后修改人Id")]
+        public Guid? LastModifyId { get; set; }
+
+        /// <summary>
+        /// 最后修改时间
+        /// </summary>
+        [DisplayName("最后修改时间")]
+        public DateTime LastModifedAt { get; set; }
+
+        /// <summary>
+        /// 是否删除
+        /// </summary>
+        [DisplayName("是否删除")]
+        public bool IsDeleted { get; set; }
+        #endregion
+    }
+
+    /// <summary>
+    /// 任务状态
+    /// </summary>
+    public enum JobStatus
+    {
+        /// <summary>
+        /// 停止
+        /// </summary>
+        Stoped = 0,
+
+        /// <summary>
+        /// 启用
+        /// </summary>
+        Enabled = 1
+
+    }
+
+    /// <summary>
+    /// 任务运行状态
+    /// </summary>
+    public enum RunStatus
+    {
+        /// <summary>
+        /// 待运行
+        /// </summary>
+        StayRun = 0,
+
+        /// <summary>
+        /// 运行中
+        /// </summary>
+        Runing = 1
+    }
+
+    /// <summary>
+    /// 触发器类型
+    /// </summary>
+    public enum TriggerType
+    {
+        /// <summary>
+        /// 简单类型触发器
+        /// </summary>
+        Simple = 0,
+
+        /// <summary>
+        /// Cron表达式
+        /// </summary>
+        Cron = 1
+    }
+}
--- src/Liliya.Test/UnitTest1.cs
+++ src/Liliya.Test/UnitTest1.cs
@@ -6,6 +6,7 @@ using Magicodes.ExporterAndImporter.Excel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SqlSugar;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -15,24 +16,59 @@ namespace Liliya.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void SqlSugarCodeFirstTest()
+        public async Task  SqlSugarCodeFirstTest()
         {
+            SqlSugarClient jobDB = new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = "server=rm-uf61ev7xoo84r6707fo.mysql.rds.aliyuncs.com;userid=ljb_ali123;pwd=Llbali@Password; database=akliajob;connectiontimeout=3000;port=3306;Pooling=true;Max Pool Size=300; Maximum Pool Size=1000;sslMode=None;AllowLoadLocalInfile=true",//连接符字串
+                DbType = DbType.MySql,// DbType.SqlServer,
+                IsAutoCloseConnection = true,
+                InitKeyType = InitKeyType.Attribute,//从特性读取主键自增信息
+            });
 
-            SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
+
+            SqlSugarClient DB = new SqlSugarClient(new ConnectionConfig()
             {
                 //ConnectionString = "Data Source=101.34.154.180,51433;Initial Catalog=LabelTest;uid=sa;pwd=Pass@Word;",//连接符字串
                 //DbType =  DbType.SqlServer,// DbType.SqlServer,
                 //IsAutoCloseConnection = true,
                 //InitKeyType = InitKeyType.Attribute,//从特性读取主键自增信息
 
-                ConnectionString = "server=rm-uf61ev7xoo84r6707fo.mysql.rds.aliyuncs.com;userid=ljb_ali123;pwd=Llbali@Password; database=liliya;connectiontimeout=3000;port=3306;Pooling=true;Max Pool Size=300; Maximum Pool Size=1000;sslMode=None;",//连接符字串
+                ConnectionString = "server=rm-uf61ev7xoo84r6707fo.mysql.rds.aliyuncs.com;userid=ljb_ali123;pwd=Llbali@Password; database=liliya;connectiontimeout=3000;port=3306;Pooling=true;Max Pool Size=300; Maximum Pool Size=1000;sslMode=None;AllowLoadLocalInfile=true",//连接符字串
                 DbType = DbType.MySql,// DbType.SqlServer,
                 IsAutoCloseConnection = true,
                 InitKeyType = InitKeyType.Attribute,//从特性读取主键自增信息
             });
 
+            DB.Aop.DataExecuting = (oldValue, entityInfo) =>
+            {
+                //添加创建时间
+                if (entityInfo.PropertyName == "CreatedAt" && entityInfo.OperationType == DataFilterType.InsertByObject)
+                {
+                    entityInfo.SetValue(DateTime.Now);
+                }
+                //添加创建人
+                if (entityInfo.PropertyName == "CreatedId" && entityInfo.OperationType == DataFilterType.InsertByObject)
+                {
+                    entityInfo.SetValue("db03fec2-f391-481b-9e24-32370a6a9942".ToGuid());
+                }
+            };
+
+            //CodeFirst测试
             //db.CodeFirst.InitTables(typeof(UserEntity));
-            db.CodeFirst.InitTables(typeof(DataDictionaryEntity));
+            //DB.CodeFirst.InitTables(typeof(DataDictionaryEntity));
+
+
+            var schedules =  await jobDB.Queryable<ScheduleEntity>().ToListAsync();
+
+
+            var startime = DateTime.Now;
+            await DB.Insertable<ScheduleEntity>(schedules).ExecuteCommandAsync();       //18813毫秒
+            //await DB.Fastest<ScheduleEntity>().PageSize(20000).BulkCopyAsync(schedules);                  //5933毫秒
+
+            var totalTime = (DateTime.Now - startime).TotalMilliseconds;
+
+            Console.WriteLine($"用时{totalTime}秒");
 
         }
 
@@ -68,9 +104,6 @@ namespace Liliya.Test
             var filebyte  = await exporter.ExportAsByteArray(users);
 
             FileHelper.CeeateFile(filePath, filebyte);
-
-            
-
         }
     }
 }
